13be0120246797ae39ff5c83c7fd439b9b2ffd5c
Danish027 <danishqes0786@gmail.com>
2024-05-28 02:14:22 +0530

    ..

--- db/migrate.ts
+++ db/migrate.ts
@@ -2,9 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const migrateClient = postgres(process.env.SUPABASE_CONNECTION_STRING!, {
-  max: 1,
-});
+const migrateClient = postgres(
+  "postgres://postgres.rrlubcsqodqplmglvket:FN801np0hNLMUPty@aws-0-ap-south-1.pooler.supabase.com:5432/postgres",
+  {
+    max: 1,
+  }
+);
 
 async function main() {
   await migrate(drizzle(migrateClient), {
